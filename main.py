09a2f064d368254173d595b7aace2277672e07c9
pahar102 <basicstore822@gmail.com>
2025-04-02 13:23:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import requests
 from telegram import Update, Bot
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
 
 # Load environment variables
 YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
@@ -53,13 +53,11 @@ def handle_message(update: Update, context: CallbackContext):
 
 # Main Function
 def main():
-    updater = Updater(TELEGRAM_BOT_TOKEN, use_context=True)
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
-    updater.start_polling()
-    updater.idle()
+    application = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    
+    application.run_polling()
 
 if __name__ == "__main__":
     main()
-      
